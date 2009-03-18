ab5e0ed06026a0601d052ac29f0cab2fe933e941
davidruan@gdnt.com.cn <davidrua@davidruan_lnx.rnd.gdnt.local>
2009-03-18 16:08:55 +0800

    import original .emacs

--- init.el
+++ init.el
@@ -17,6 +17,8 @@
 (add-to-list 'load-path dotfiles-dir)
 (add-to-list 'load-path (concat dotfiles-dir "/elpa-to-submit"))
 (add-to-list 'load-path (concat dotfiles-dir "/elpa-to-submit/jabber"))
+(add-to-list 'load-path (concat dotfiles-dir "/elpa-to-submit/ecb-2.32"))
+(add-to-list 'load-path (concat dotfiles-dir "/elpa-to-submit/cedet-1.0pre6/common"))
 
 (setq autoload-file (concat dotfiles-dir "loaddefs.el"))
 (setq package-user-dir (concat dotfiles-dir "elpa"))
@@ -31,7 +33,7 @@
 (require 'uniquify)
 (require 'ansi-color)
 (require 'recentf)
-
+(require 'idle-highlight)
 ;; this must be loaded before ELPA since it bundles its own
 ;; out-of-date js stuff. TODO: fix it to use ELPA dependencies
 (load "elpa-to-submit/nxhtml/autostart")
@@ -78,3 +80,43 @@
   (mapc #'load (directory-files user-specific-dir nil ".*el$")))
 
 ;;; init.el ends here
+
+;; Load CEDET.
+;; See cedet/common/cedet.info for configuration details.
+;;(load-file "~/.emacs.d/elpa-to-submit/cedet-1.0pre6/common/cedet.el")
+(require 'cedet)
+
+;; Enable EDE (Project Management) features
+(global-ede-mode 1)
+
+;; Enable EDE for a pre-existing C++ project
+;; (ede-cpp-root-project "NAME" :file "~/myproject/Makefile")
+
+
+;; Enabling Semantic (code-parsing, smart completion) features
+;; Select one of the following:
+
+;; * This enables the database and idle reparse engines
+(semantic-load-enable-minimum-features)
+
+;; * This enables some tools useful for coding, such as summary mode
+;;   imenu support, and the semantic navigator
+(semantic-load-enable-code-helpers)
+
+;; * This enables even more coding tools such as intellisense mode
+;;   decoration mode, and stickyfunc mode (plus regular code helpers)
+;; (semantic-load-enable-gaudy-code-helpers)
+
+;; * This enables the use of Exuberent ctags if you have it installed.
+;;   If you use C++ templates or boost, you should NOT enable it.
+;; (semantic-load-enable-all-exuberent-ctags-support)
+
+;; Enable SRecode (Template management) minor-mode.
+;; (global-srecode-minor-mode 1)
+
+;;ECB
+;;(add-to-list 'load-path
+;;    "/localdisk/data/download/ecb-2.32")
+
+(require 'ecb)
+
