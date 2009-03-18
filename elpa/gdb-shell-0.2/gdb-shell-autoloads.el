e79db8d6edbf8796f050da0418db31412bb848db
David Ruan <david@david-desktop.(none)>
2009-03-18 22:07:32 +0800

    add files in elpa

---
+++ elpa/gdb-shell-0.2/gdb-shell-autoloads.el
@@ -0,0 +1,26 @@
+;;; gdb-shell-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (gdb-shell-minor-mode) "gdb-shell" "gdb-shell.el"
+;;;;;;  (18880 65156))
+;;; Generated autoloads from gdb-shell.el
+
+(autoload (quote gdb-shell-minor-mode) "gdb-shell" "\
+Minor mode to add gdb features to shell mode.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("gdb-shell-pkg.el") (18880 65156 52993))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; gdb-shell-autoloads.el ends here
---
+++ elpa/yasnippet-bundle-0.5.10/yasnippet-bundle-autoloads.el
@@ -0,0 +1,23 @@
+;;; yasnippet-bundle-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil "yasnippet-bundle" "yasnippet-bundle.el" (18880
+;;;;;;  65100))
+;;; Generated autoloads from yasnippet-bundle.el
+(require 'yasnippet-bundle)
+
+;;;***
+
+;;;### (autoloads nil nil ("yasnippet-bundle-pkg.el") (18880 65100
+;;;;;;  302908))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; yasnippet-bundle-autoloads.el ends here
